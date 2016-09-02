2ca90118740319f2b25f1d71a59f76d2442cb21a
HarishGH <hkumar_k@yahoo.com>
2016-09-02 15:05:08 -0400

    My changes to 2nd Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -13,14 +13,16 @@ list(set=set, get=get,
    getmatrix=getmatrix)
 }
 
-cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
-    }
-    datos<-x$get()
-    m<-solve(datos, ...)
-    x$setmatrix(m)
-    m
+
+cacheSolve <- function(x, ...) {
+  m <- x$getInverse()                 
+  if(!is.null(m)){                    
+    message("getting cached data")   
+    return(m)                         
+  }
+  data <- x$get()                      
+  m <- solve(data, ...)               
+  x$setInverse(m)                    
 }
+
+  
